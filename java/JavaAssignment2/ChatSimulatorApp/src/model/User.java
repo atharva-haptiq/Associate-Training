dc344066e9b2f04ad561381fd47fa64cfb778a3c
atharva-haptiq <atharva.kakade@haptiq.com>
2025-07-22 15:23:12 +0530

    Chat simulator refreshed

--- java/JavaAssignment2/ChatSimulatorApp/src/model/User.java
+++ java/JavaAssignment2/ChatSimulatorApp/src/model/User.java
@@ -1,14 +1,15 @@
 package model;
 
-public class User {
-    String username;
+import java.util.Objects;
+
+public class User implements Comparable<User> {
+    private String username;
 
     public User(String username) {
         this.username = username;
     }
 
-    public User() {
-    }
+    public User() {}
 
     public String getUsername() {
         return username;
@@ -16,5 +17,28 @@ public class User {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    @Override
+    public int compareTo(User other) {
+        return this.username.compareToIgnoreCase(other.username);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return username.equalsIgnoreCase(user.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username.toLowerCase());
+    }
+
+    @Override
+    public String toString() {
+        return username;
     }
 }
